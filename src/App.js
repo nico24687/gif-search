0c1aedd04aab3ca95885bb60d5df96bf46682723
nico24687 <theonlynico@hotmail.com>
2018-05-17 18:36:36 -0600

    now using axios to interact with the giphy api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
 import SearchForm from './Components/SearchForm';
 import GifList from './Components/GifList';
 
@@ -12,11 +13,23 @@ export default class App extends Component {
     }
   } 
 
-  componentDidMount(){
-    fetch('https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
-      .then(response => response.json())
-      .then(responseData => {this.setState({gifs: responseData.data})})
-      .catch(error => console.log(error))  
+  // componentDidMount(){
+  //   fetch('https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+  //     .then(response => response.json())
+  //     .then(responseData => {this.setState({gifs: responseData.data})})
+  //     .catch(error => console.log(error))  
+  // }
+
+  componentDidMount() {
+    axios.get('https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+      // here axios does the conversion into Json for you
+      .then(response => {
+        this.setState({ gifs: response.data.data })
+        //response.data because of the axios schema and then data again to get the info we want
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data');
+      });
   }
 
   render() { 
