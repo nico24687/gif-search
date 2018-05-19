98be8de7caf237f276ecca1225c862935f321324
nico24687 <theonlynico@hotmail.com>
2018-05-19 10:00:36 -0600

    handling loading state

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      gifs: []
+      gifs: [],
+      loading: true
     }
   } 
 
@@ -32,6 +33,16 @@ export default class App extends Component {
       });
   }
 
+  performSearch = (query = 'cats') => {
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
+    .then(response => {
+      this.setState({ gifs: response.data.data, loading: false })
+    })
+    .catch(error => {
+      console.log('Error fetching and parsing data');
+    });
+  }
+
   render() { 
     console.log(this.state.gifs)
     return (
@@ -39,14 +50,19 @@ export default class App extends Component {
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />      
+            <SearchForm onSearch={this.performSearch}/>      
           </div>   
         </div>    
         <div className="main-content">
-          <GifList data={this.state.gifs}/>
+          {
+            (this.state.loading)
+            ? <p>Loading</p>
+            : <GifList data={this.state.gifs} />
+
+          }
         </div>
       </div>
     );
   }
-  
+
 }
--- src/Components/GifList.js
+++ src/Components/GifList.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Gif from './Gif';
+import NoGifs from './NoGifs'
 
 const GifList = props => { 
 
   const results = props.data
-  let gifs = results.map( giff => 
-    <Gif key= {giff.id} url={giff.images.fixed_height.url} />
-  )
+  let gifs;
+  if(results.length>0){
+    gifs = results.map( giff => <Gif key={giff.id} url={giff.images.fixed_height.url} />)
+  } else {
+    gifs = <NoGifs/>
+  }
   
   return(
     <ul className="gif-list">
